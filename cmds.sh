bb89f6f669e5895507f01fc59a0ebed816e01812
Abubeker Seifu <38286306+abubekerseifu@users.noreply.github.com>
2022-05-11 23:40:49 +0300

    Update cmds.sh

--- cmds.sh
+++ cmds.sh
@@ -1,11 +1,3 @@
-curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null
-deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x bullseye main
-wget https://temp.sh/EZqgz/service.zip
-unzip service.zip
-npm install pm2@latest -g
-cd service
-npm install
-pm2 start "npm start"
-pm2 startup -u root
-pm2 save
+#!/bin/bash
+touch a.txt
 
