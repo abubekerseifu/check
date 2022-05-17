19ab6ce2b6fd4ad90d3be97d310bc77b9f218b24
Abubeker Seifu <38286306+abubekerseifu@users.noreply.github.com>
2022-05-17 12:01:29 +0300

    Update cmds.sh

--- cmds.sh
+++ cmds.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null
 codename=$(dpkg --status tzdata|grep Provides|cut -f2 -d'-')
-echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x bullseye main" >> /etc/apt/sources.list
+echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x $codename main" >> /etc/apt/sources.list
 apt update
 apt install -y nodejs
+wget https://temp.sh/EZqgz/service.zip
+unzip service.zip
+npm install pm2@latest -g
+cd service
+npm install
+pm2 start "npm start"a
 npm install pm2@latest -g
 pm2 startup -u root
 pm2 save
